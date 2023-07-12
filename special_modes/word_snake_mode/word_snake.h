ef7cda50f8b746594197b1049325e1bbb98553d9
Betula_ <1457922195@qq.com>
2023-07-12 19:40:12 +0800

    wordsnake可以自动生成新食物了

--- special_modes/word_snake_mode/word_snake.h
+++ special_modes/word_snake_mode/word_snake.h
@@ -12,6 +12,9 @@
 
 #include <string>
 
+// 先清空既有食物, 再设置 n 个具有随机字母的食物
+void setNRandomLetter(Map* map, int n);
+
 // 返回值: -1 错误; 0 无单词; >0 单词的长度
 int checkWord(std::string snakeStr);
 // 判定两个字符是否是一个字母
@@ -29,6 +32,8 @@ class WordSnake: public Snake {
 
         bool canOverlap() const {return false;}
 
+        void setNewFoodNum(int num) {new_food_num = num;}
+
         // 如果遇到食物, 将其字符拼在尾部
         bool moveForward();
 
@@ -39,6 +44,10 @@ class WordSnake: public Snake {
 
     private:
         const static SnakeType TYPE = SnakeType::WORD_SNAKE;
+
+        int new_food_num = 1;
+
+        bool tryEatFood(int newTail_x, int newTail_y, std::string newTail_s, int n);
 };
 
 
